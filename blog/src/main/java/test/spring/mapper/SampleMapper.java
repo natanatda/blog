7608514b578bc478e7da7dadd7a56d5665c43848
natanatda <codesjj9@gmail.com>
2023-06-08 14:44:58 +0900

    ㅇ

--- blog/src/main/java/test/spring/mapper/SampleMapper.java
+++
@@ -1,9 +0,0 @@
-package test.spring.mapper;
-
-import java.util.List;
-
-import test.spring.component.SampleDTO;
-
-public interface SampleMapper {
-	public List<SampleDTO> sampleTest();
-}
--- blog/src/main/java/test/spring/service/SampleService.java
+++
@@ -1,9 +0,0 @@
-package test.spring.service;
-
-import java.util.List;
-
-import test.spring.component.SampleDTO;
-
-public interface SampleService {
-	public List<SampleDTO> sampleTest();
-}
